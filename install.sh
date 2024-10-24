15535377e05dab431eaad4503d18c26d3c467da4
rcv11x <alejandrooymariaa1@gmail.com>
2024-10-24 13:35:45 +0200

    +

---
+++ install.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+source "scripts/utils.sh"
+source "scripts/gpu-drivers.sh"
+source "scripts/multimedia.sh"
+
+trap stop_script INT
+
+function show_banner() {
+                                                         
+    echo -e "  _____        _                 _____              _      "
+    echo -e " |  ___|__  __| | ___  _ __ __ _|  ___| __ ___  ___| |__   "
+    echo -e " | |_ / _ \/ _' |/ _ \| '__/ _' | |_ | '__/ _ \/ __| '_ \  "
+    echo -e " |  _|  __/ (_| | (_) | | | (_| |  _|| | |  __/\__ \ | | | "
+    echo -e " |_|  \___|\__,_|\___/|_|  \__,_|_|  |_|  \___||___/_| |_| \n"
+    echo -e "Fedora: ${fedora_variant} v${fedora_version} - \n\n" 
+
+}
+
+function menu() {
+
+    echo -e "(1) ${cyan}Install FedoraFresh${resetStyle}"
+    echo -e "(2) ${cyan}Install GPU drivers (Nvidia e Intel)${resetStyle}"
+    echo -e "(3) ${cyan}Install Multimedia${resetStyle}"
+    echo -e "(4) ${cyan}Install Flatpak's${resetStyle}"
+    echo -e "(0) ${cyan}Exit${resetStyle}\n"
+}
+
+function install() {
+    clear
+    echo -e "--> Empezando la instalacion ;)"; sleep 2
+    clear
+    echo -e "${purple}[!] Configurando DNF...${resetStyle}"; sleep 1
+    #echo "max_parallel_downloads=10" | sudo tee -a /etc/dnf/dnf.conf > /dev/null
+    checkRPMfusion
+    #installMultimedia
+    sleep 1
+
+
+}
+
+function init(){
+
+    
+    clear
+    if [[ $(id -u) = 0 || $(whoami) = "root" ]]; then
+
+        echo -e "\n${red}[!] Ejecuta el script sin sudo\n${resetStyle}"
+        exit 1
+    else
+        while true; do
+
+            show_banner
+            menu
+
+            read -p "$(whoami) >> " opcion
+            case $opcion in 
+                1)
+                    install
+                    ;;
+                2)
+                    clear
+                    install_multimedia
+                    ;;
+                3)
+                    clear
+                    install_flatpaks
+                    ;;
+                0)
+                    exit 0
+                    ;;
+                *) 
+                    echo -e "\n${red}[!] Opción no válida${resetStyle}"
+                    echo -e "\nPresiona una tecla para continuar"
+                    read -n 1 -s -r -p ""
+                    clear
+                    ;;
+            esac
+        done
+    fi
+    
+}
+
+init
---
+++ scripts/utils.sh
@@ -0,0 +1,122 @@
+#!/bin/bash
+
+source "../script.sh"
+
+# -- Colors -- #
+
+resetStyle="\e[0m"
+red="\e[0;31m"
+blue="\e[0;34m"
+cyan="\e[0;36m"
+green="\e[0;32m"
+yellow="\e[0;33m"
+purple="\e[0;35m"
+white="\e[0;37m"
+black="\e[0;30m"
+fedora_version=$(cat /etc/os-release | grep -i "VERSION_ID" | awk -F'=' '{print $2}')
+fedora_variant=$(cat /etc/os-release | grep -w "VARIANT" | awk -F'=' '{print $2}' | sed 's/"//g')
+
+function stop_script() {
+    clear
+    echo -e "\n${red}[!] Has salido del script \n${resetStyle}"
+    exit 1
+}
+
+function msg_ok() {
+    echo -e "${red}[${white} OK ${red}] ${resetStyle}\n"
+}
+
+function bannerText() {
+
+    echo -e "=================================================================================="
+    echo -e "=                                                                                 "
+    echo -e "=   $1                                                                              " 
+    echo -e "=                                                                                 "
+    echo -e "=================================================================================="
+}
+
+function checkRPMfusion() {
+    if [[ -f /etc/yum.repos.d/rpmfusion-free.repo || -f /etc/yum.repos.d/rpmfusion-nonfree.repo ]]; then
+        echo
+        bannerText "${yellow}El repositorio de RPM Fusion ya está instalado.${resetStyle}"
+        echo -e "$(msg_ok) Omitiendo...\n"
+        sleep 5
+    else
+        echo
+        bannerText "${yellow}Añadiendo el repositorio de RPM Fusion...${resetStyle}"
+        sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-"$(rpm -E %fedora)".noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-"$(rpm -E %fedora)".noarch.rpm > /dev/null 2>&1
+        echo -e "$(msg_ok) Listo\n"
+        sleep 5
+    fi
+}
+
+function check_intel_or_amd_cpu() {
+
+    if grep -q "Intel" /proc/cpuinfo; then
+
+        bannerText "${yellow}[!] CPU Intel detectado, instalando codecs necesarios...${resetStyle}"
+        sudo dnf swap libva-intel-media-driver intel-media-driver --allowerasing
+
+    elif grep -q "AMD" /proc/cpuinfo; then
+
+        bannerText "${yellow}[!]CPU AMD detectado, instalando codecs necesarios...${resetStyle}"
+        sudo dnf swap mesa-va-drivers mesa-va-drivers-freeworld
+        sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+    else
+        echo "${yellow}[Error]${resetStyle} CPU no reconocido"
+        
+    fi
+
+}
+
+function installMultimedia() {
+
+    clear
+    bannerText "Por defecto Fedora ${fedora_version} proporciona unicamente Codecs free que suelen carecer de muchas funcionalidades y soporte, a continuación se instalarán los Codecs Multimedia completos para un buen funcionamiento y soporte, para ello hay que añadir un repositorio de terceros llamado RPM Fusion ¿Quieres continuar? [y/n]"
+    read -p ">> " opt
+    while true; do
+        case $opt in 
+            y) 
+
+                checkRPMfusion
+                sudo dnf swap 'ffmpeg-free' 'ffmpeg' --allowerasing
+                sudo dnf group install Multimedia
+                sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin
+                sudo dnf update @sound-and-video
+                sleep 3
+                echo -e "\n${purple}[!] Instalando codecs para la Decodificacion de Video...${resetStyle}\n"
+                sudo dnf install ffmpeg ffmpeg-libs libva libva-utils
+                check_intel_or_amd
+                echo -e "\n${purple}[!] Configurando OpenH264 para Firefox...${resetStyle}\n"
+                sudo dnf config-manager --set-enabled fedora-cisco-openh264
+                sudo dnf install -y openh264 gstreamer1-plugin-openh264 mozilla-openh264
+                ;;
+            n)
+                ;;
+
+            *) 
+                    echo -e "\n${red}[!] Opción no válida${resetStyle}"
+                    echo -e "\nPresiona una tecla para continuar"
+                    read -n 1 -s -r -p ""
+                    clear
+                    ;;
+        esac
+    done
+
+}
+
+# Detectar las GPU's instaladas en el sistema
+gpu_info=$(lspci | grep -i 'vga\|3d\|2d' | awk -F': ' '{print $2}')
+
+echo "GPUs detectadas: "
+echo "$gpu_info"
+
+if echo "$gpu_info" | grep -i 'nvidia' > /dev/null; then
+    echo "Se ha detectado una GPU NVIDIA."
+    sudo dnf install akmod-nvidia xorg-x11-drv-nvidia-cuda
+fi
+
+if echo "$gpu_info" | grep -i 'intel' > /dev/null; then
+    echo "Se ha detectado una iGPU Intel."
+    # 
+fi
